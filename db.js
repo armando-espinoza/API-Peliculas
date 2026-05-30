267a57fe3580bf3eb72347c6ede59a0ba82d8f44
Armando <ja.gamez24@info.uas.edu.mx>
2026-05-29 21:56:27 -0700

    Consumo a una bd publica con postgres

--- db.js
+++ db.js
@@ -1,24 +1,32 @@
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'database.sqlite'
+if (!process.env.DATABASE_URL) {
+  throw new Error("FATAL ERROR: DATABASE_URL no está definida en las variables de entorno.");
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: false
 });
 
-async function conectar(){
-  try{
+async function conectar() {
+  try {
     await sequelize.authenticate();
-    console.log('Base de datos conectada');
+    console.log('Conexión a PostgreSQL establecida con éxito.');
     await sequelize.sync();
-  }
-  catch(error){
-    console.error( 'No se ha logrado conectar la base de datos' );
+  } catch (error) {
+    console.error('No se pudo conectar a la base de datos:', error.message);
   }
 }
 
 conectar();
 
 module.exports = sequelize;
-
-
-
